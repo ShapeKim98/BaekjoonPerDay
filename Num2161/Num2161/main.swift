c32b1566e68681965d5901cd61e98fcf392fac96
ShapeKim98 <talkers.potters-0p@icloud.com>
2023-01-19 22:45:08 +0900

    add Num2161

---
+++ Num2161/Num2161/main.swift
@@ -0,0 +1,93 @@
+//
+//  main.swift
+//  Num2161
+//
+//  Created by 김도형 on 2023/01/19.
+//
+
+import Foundation
+
+final class FileIO {
+    private let buffer:[UInt8]
+    private var index: Int = 0
+
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        
+        buffer = Array(try! fileHandle.readToEnd()!)+[UInt8(0)] // 인덱스 범위 넘어가는 것 방지
+    }
+
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+
+        return buffer[index]
+    }
+
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+
+        while now == 10
+                || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        if now == 45 { isPositive.toggle(); now = read() } // 음수 처리
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+
+        return sum * (isPositive ? 1:-1)
+    }
+
+    @inline(__always) func readString() -> String {
+        var now = read()
+
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        let beginIndex = index-1
+
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return String(bytes: Array(buffer[beginIndex..<(index-1)]), encoding: .ascii)!
+    }
+
+    @inline(__always) func readByteSequenceWithoutSpaceAndLineFeed() -> [UInt8] {
+        var now = read()
+
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        let beginIndex = index-1
+
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return Array(buffer[beginIndex..<(index-1)])
+    }
+}
+
+let fileIO = FileIO()
+let n = fileIO.readInt()
+
+var cards = [Int?]()
+for num in 1...n {
+    cards.append(num)
+}
+
+var answer = ""
+var index = 0
+while cards.count > index + 1 {
+    answer += "\(cards[index] ?? 0) "
+    cards[index] = nil
+    let num = cards[index + 1]
+    cards[index + 1] = nil
+    cards.append(num)
+    index += 2
+}
+
+cards.forEach { num in
+    if let value = num {
+        answer += "\(value)"
+    }
+}
+
+print(answer, terminator: "")
